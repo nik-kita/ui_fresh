e2583d8f6be8ac8a0878879d67dd93d78567d9e8
nikita <nik.moiseienko@gmail.com>
2023-11-15 00:43:33 +0100

    uncontrolled react form + update state for arrays

--- islands/Chat.tsx
+++ islands/Chat.tsx
@@ -1,5 +1,8 @@
-import { useState } from "preact/hooks";
+import { useRef, useState } from "preact/hooks";
 import { useWs } from "./hooks/use-ws/hook.ts";
+import { JSX } from "preact/jsx-runtime";
+
+let messages_id = 0;
 
 export default function Chat({
   connection_url,
@@ -9,15 +12,30 @@ export default function Chat({
     should_be: "connected",
   });
   const [messages, set_messages] = useState<string[]>([]);
+  const form_ref = useRef<HTMLFormElement>(null);
+  const input_message_ref = useRef<HTMLInputElement>(null);
+  const handle_form_submit: JSX.GenericEventHandler<HTMLFormElement> = (ev) => {
+    ev.preventDefault();
 
-  ws.add("on", "message", ({ data }) => set_messages([...messages, data]));
+    const message = input_message_ref.current!.value;
+
+    if (message) {
+      ws.send(message.trim().replaceAll("  ", " ").replaceAll("\n\n", "\n"));
+      input_message_ref.current!.value = "";
+    }
+  };
+
+  ws.add("on", "message", ({ data }) => {
+    set_messages((prev) => [...prev, data]);
+    console.log(messages);
+  });
 
   return (
     <div>
       <ul>
-        {messages.map((m, i) => {
+        {...messages.map((m) => {
           return (
-            <li key={i}>
+            <li key={++messages_id}>
               <pre>
               {m}
               </pre>
@@ -26,8 +44,12 @@ export default function Chat({
         })}
       </ul>
       <hr />
-      <form>
-        <input type="text" />
+      <form ref={form_ref} onSubmit={handle_form_submit}>
+        <input
+          ref={input_message_ref}
+          type="text"
+          placeholder={"write message..."}
+        />
         <input type="submit" value="send" />
       </form>
     </div>
