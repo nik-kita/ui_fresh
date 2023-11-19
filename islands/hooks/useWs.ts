9b89cc71f3bc33c2a77c2ae7fce084c722dd9919
nikita <nik.moiseienko@gmail.com>
2023-11-19 03:44:30 +0100

    commit

--- islands/hooks/useWs.ts
+++ islands/hooks/useWs.ts
@@ -15,8 +15,14 @@ const READY_STATE = {
 } as const;
 
 export function useWs(url: string) {
-  const engine = useRef({
+  const x = useRef({
     ws: null as null | SugarWs,
+    listeners: new Map<string, {
+      on_or_once: "on" | "once";
+      label: string;
+      listener: EvL;
+      options?: boolean | AddEventListenerOptions;
+    }>(),
   });
   const [
     on_or_off,
@@ -34,11 +40,48 @@ export function useWs(url: string) {
   return {
     is_online,
     turn: set_or_on_off,
-    on(...args: Parameters<SugarWs["on"]>) {
+    on([label, listener, options]: Parameters<SugarWs["once"]>) {
+      const key = `on::${label}::${listener.toString()}`;
+
+      if (x.current.listeners.has(key)) {
+        x.current.listeners.set(key, {
+          label,
+          listener,
+          on_or_once: "on",
+          options,
+        });
+      }
+
+      return key;
     },
-    once(...args: Parameters<SugarWs["once"]>) {
+    once([label, listener, options]: Parameters<SugarWs["once"]>) {
+      const key = `once::${label}::${listener.toString()}`;
+
+      if (!x.current.listeners.has(key)) {
+        x.current.listeners.set(key, {
+          label,
+          listener,
+          on_or_once: "once",
+          options,
+        });
+      }
+
+      return key;
     },
-    rm_listener() {
+    rm(key: string) {
+      const rm_target = x.current.listeners.get(key);
+
+      if (!rm_target) return false;
+
+      x.current.listeners.delete(key);
+      x.current.ws?.removeEventListener(
+        rm_target.label,
+        rm_target.listener as EventListenerOrEventListenerObject,
+      );
+
+      return true;
     },
   };
 }
+
+type EvL = Parameters<SugarWs["once"]>[1];
